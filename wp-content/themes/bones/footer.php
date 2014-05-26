495473d26e647711553ead4072f512494790d9a0
Rachel Flowers <rachel@appicstudios.com>
2014-05-26 19:54:11 -0400

    site styles

--- wp-content/themes/bones/footer.php
+++ wp-content/themes/bones/footer.php
@@ -1,8 +1,9 @@
 			<footer class="footer" role="contentinfo">
 
 				<div id="inner-footer" class="wrap cf">
+				    
 
-					<nav role="navigation">
+					<nav id="footer-nav" role="navigation">
 						<?php wp_nav_menu(array(
     					'container' => '',                              // remove nav container
     					'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
@@ -18,13 +19,15 @@
 						)); ?>
 					</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
-
-				</div>
-
-			</footer>
-
-		</div>
+    				<div id="footer-contact">
+    					<h3>Contact</h3>
+    					<p><img src="<?php echo get_template_directory_uri(); ?>/library/images/phone.png" alt="Call Us" /> 517.669.HOME (4663)</p>
+    					<p><img src="<?php echo get_template_directory_uri(); ?>/library/images/email.png" alt="Email Us" /> pvbuilder@aol.com</p>
+    					<p class="source-org copyright">Copyright &copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> LLC. All Rights Reserved.</p>
+    				</div>
+			</div>
+            <p class="pixelplay clear">Website brought to you by: <a href="http://www.pixelplaycreative.com">Pixel Play Creative</a></p>
+		</footer>
 
 		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
--- wp-content/themes/bones/header.php
+++ wp-content/themes/bones/header.php
@@ -47,14 +47,12 @@
 
 				<div id="inner-header" class="wrap cf">
 
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<a href="<?php echo home_url(); ?>" id="logo" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" alt="Pete Vandenberg Builder" /></a>
 
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
+					<h1><?php echo bloginfo('description'); ?></h1>
 
 
-					<nav role="navigation">
+					<nav id="main-nav" role="navigation">
 						<?php wp_nav_menu(array(
     					'container' => false,                           // remove nav container
     					'container_class' => 'menu cf',                 // class of container (should you choose to use it)
