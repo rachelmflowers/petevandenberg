852e44f3abad29456487939d419f514eca53f6ef
Rachel Flowers <rachel@appicstudios.com>
2014-06-04 18:36:26 -0400

    fixes

--- wp-content/themes/bones/functions.php
+++ wp-content/themes/bones/functions.php
@@ -70,7 +70,7 @@ if ( ! isset( $content_width ) ) {
 /************* THUMBNAIL SIZE OPTIONS *************/
 
 // Thumbnail sizes
-add_image_size( 'bones-thumb-600', 600, 150, true );
+add_image_size( 'bones-thumb-900', 900, 350, true );
 add_image_size( 'bones-thumb-300', 300, 100, true );
 
 /*
@@ -97,7 +97,7 @@ add_filter( 'image_size_names_choose', 'bones_custom_image_sizes' );
 
 function bones_custom_image_sizes( $sizes ) {
     return array_merge( $sizes, array(
-        'bones-thumb-600' => __('600px by 150px'),
+        'bones-thumb-900' => __('900px by 350px'),
         'bones-thumb-300' => __('300px by 100px'),
     ) );
 }
